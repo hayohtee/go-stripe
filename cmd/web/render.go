29ed772b613996a65195532d4ee22d641792dd3c
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-22 14:47:13 +0100

    Add new methods

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"html/template"
+	"net/http"
+	"strings"
 )
 
 type templateData struct {
@@ -23,3 +26,70 @@ var functions = template.FuncMap{}
 
 //go:embed templates
 var templateFS embed.FS
+
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	return td
+}
+
+func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
+	var t *template.Template
+	var err error
+	templateName := fmt.Sprintf("templates/%s_page.tmpl", page)
+
+	_, ok := app.templateCache[templateName]
+	if app.cfg.env == "production" && ok {
+		t = app.templateCache[templateName]
+	} else {
+		t, err = app.parseTemplate(partials, page, templateName)
+		if err != nil {
+			app.errorLog.Println(err)
+			return err
+		}
+	}
+
+	if td == nil {
+		td = &templateData{}
+	}
+
+	td = app.addDefaultData(td, r)
+
+	err = t.Execute(w, td)
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (app *application) parseTemplate(partials []string, page, templateName string) (*template.Template, error) {
+	var t *template.Template
+	var err error
+
+	if len(partials) > 0 {
+		for i, x := range partials {
+			partials[i] = fmt.Sprintf("templates/%s_partial.tmpl", x)
+		}
+
+		t, err = template.New(fmt.Sprintf("%s_page.tmpl", page)).Funcs(functions).ParseFS(
+			templateFS,
+			"templates/base_layout.tmpl",
+			strings.Join(partials, ","),
+			templateName,
+		)
+	} else {
+		t, err = template.New(fmt.Sprintf("%s_page.tmpl", page)).Funcs(functions).ParseFS(
+			templateFS,
+			"templates/base_layout.tmpl",
+			templateName,
+		)
+	}
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return nil, err
+	}
+
+	app.templateCache[templateName] = t
+	return t, nil
+}
